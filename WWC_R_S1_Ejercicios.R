44f9b61e23788a6483733b1c4f00302db819944b
shirley gomez <shirleycr2018@gmail.com>
2022-08-28 15:58:27 -0600

    test

--- WWC_R_S1_Ejercicios.R
+++ WWC_R_S1_Ejercicios.R
@@ -1,4 +1,4 @@
 # Este archivo contiene los ejercicios de la clase 1 de curso basico R 
 url <- "https://raw.githubusercontent.com/shirleycr2018/WWC_R_S1/575b0da6b4b16473d295380a1a43ee4d34f49989/mr.csv"
-
+# read file from URL 
 data <- read.csv(url, strip.white = TRUE)
